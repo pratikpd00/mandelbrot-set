fbf53b6214ecd91d59deb467dde56163773a3065
pratik <pratikpd00@gmail.com>
2024-08-11 21:45:01 -0400

    write cuda algorithm

--- src/mandelbrot_algorithms/escapeTimeCuda.cu
+++ src/mandelbrot_algorithms/escapeTimeCuda.cu
@@ -1,14 +1,28 @@
-__global__ void escapeTime(int max_iters, float scale, float panX, float panY, int** escapeTimes) {
+__global__ void escapeTime(int** escapeTimes, int max_iters, double scale, double panX, double panY) {
 	//Mandelbrot recurrence for reference: z_n+1 = z_n^2 + c
-	float cReal = threadIdx.x * scale + panX;
-	float cImaginary = threadIdx.y + panY;
+	const double cReal = threadIdx.x * scale + panX;
+	const double cImaginary = threadIdx.y + panY;
 
-	float zReal = 0;
-	float zImaginary = 0;
+	double zReal = 0;
+	double zImaginary = 0;
 
-	for (int i = 0; i < max_iters && zReal ^ 2 + zImaginary ^ 2 <= 4; i++) {
+	double zRealSqr = 0;
+	double zImaginarySqr = 0;
 
+	//if the absolute value of z ever exceeds 2, it is guaranteed to escape the mandelbrot set
+	int i;
+	for (i = 0; i < max_iters && zRealSqr + zImaginarySqr <= 4; i++) {
+		//(ai + b) ^2 = -a^2 + 2abi + b^2
+		zImaginary = 2*zReal*zImaginary + cImaginary;
+		zReal = zRealSqr - zImaginarySqr + cReal;
+		
+		zRealSqr = zReal*zReal;
+		zImaginarySqr = zImaginary*zImaginary;
 	}
 
+	escapeTimes[threadIdx.x][threadIdx.y] = i; 
+}
+
+void CudaEscapeTime() {
 
 }
