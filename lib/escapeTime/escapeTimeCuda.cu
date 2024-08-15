41e426fc79f371e28565ec3dc033817ad1208a11
pratik <pratikpd00@gmail.com>
2024-08-15 01:29:52 -0400

    Change escape time algorithm to use 1d pointer

--- lib/escapeTime/escapeTimeCuda.cu
+++ lib/escapeTime/escapeTimeCuda.cu
@@ -1,9 +1,17 @@
+#include <math.h>
 #include "escapeTimeCuda.cuh"
 
-__global__ void escapeTime(int** escapeTimes, int max_iters, double scale, double panX, double panY) {
+__global__ void escapeTime(int* escapeTimes, int sizeX, int sizeY, int max_iters, double scale, double panX, double panY) {
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (x >= sizeX || y >= sizeY) {
+		return;
+	}
+	
 	//Mandelbrot recurrence for reference: z_n+1 = z_n^2 + c
-	const double cReal = threadIdx.x * scale + panX;
-	const double cImaginary = threadIdx.y + panY;
+	const double cReal = x * scale + panX;
+	const double cImaginary = y * scale + panY;
 
 	double zReal = 0;
 	double zImaginary = 0;
@@ -11,7 +19,7 @@ __global__ void escapeTime(int** escapeTimes, int max_iters, double scale, doubl
 	double zRealSqr = 0;
 	double zImaginarySqr = 0;
 
-	//if the absolute value of z ever exceeds 2, it is guaranteed to escape the mandelbrot set
+	//if the absolute value of z ever exceeds 2, it is guaranteed to escape the mandelbrot set. Therefore, this loop can be terminated
 	int i;
 	for (i = 0; i < max_iters && zRealSqr + zImaginarySqr <= 4; i++) {
 		//(ai + b) ^2 = -a^2 + 2abi + b^2
@@ -22,10 +30,15 @@ __global__ void escapeTime(int** escapeTimes, int max_iters, double scale, doubl
 		zImaginarySqr = zImaginary*zImaginary;
 	}
 
-	escapeTimes[threadIdx.x][threadIdx.y] = i; 
+	escapeTimes[x * sizeX + y] = i; 
 }
 
-void CudaEscapeTime(int** escapeTimes, int max_iters, int sizeX, int sizeY, double scale, double panX, double panY) {
-	dim3 threads(sizeX, sizeY);
-	escapeTime<<<1, threads>>>(escapeTimes, max_iters, scale, panX, panY);
+void CudaEscapeTime(int* escapeTimes, int max_iters, int sizeX, int sizeY, double scale, double panX, double panY) {
+	dim3 threads(16, 16);
+	int blockXNum = ceil(sizeX/threads.x);
+	int blockYNum = ceil(sizeY/threads.y);
+	dim3 blocks(blockXNum, blockYNum);
+	int* cudaEscapeTimes;
+	cudaMalloc(&cudaEscapeTimes, sizeof(*cudaEscapeTimes) * sizeX * sizeY);
+	escapeTime<<<blocks, threads>>>(escapeTimes, max_iters, sizeX, sizeY, scale, panX, panY);
 }
--- lib/escapeTime/escapeTimeCuda.cuh
+++ lib/escapeTime/escapeTimeCuda.cuh
@@ -2,6 +2,6 @@
 #define ESCAPE_TIME_CUDA
 
 #include "escapeTime/escapeTimeCuda.h"
-__global__ void escapeTime(int** escapeTimes, int max_iters, double scale, double panX, double panY);
+__global__ void escapeTime(int** escapeTimes, int max_iters, int sizeX, int sizeY, double scale, double panX, double panY);
 
 #endif 
