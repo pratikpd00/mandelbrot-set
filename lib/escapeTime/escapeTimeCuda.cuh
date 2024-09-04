1daa9f84704876aee093a280ee32ee436a695d97
pratikpd00 <pratikpd00@gmail.com>
2024-09-04 18:17:45 -0400

    Add preprocessor macro for benchmarking

--- lib/escapeTime/escapeTimeCuda.cuh
+++ lib/escapeTime/escapeTimeCuda.cuh
@@ -6,3 +6,17 @@
 __global__ void escapeTime(int* escapeTimes, int max_iters, int sizeX, int sizeY, double scale, double panX, double panY);
 
 #endif 
+
+#ifdef BENCHMARKING
+#include <chrono>
+
+#define RUN_ESCAPE_TIME_KERNEL auto start = std::chrono::steady_clock::now(); \
+				escapeTime <<<blocks, threads >>> (cudaEscapeTimes, maxIters, sizeX, sizeY, scale, panX, panY); \
+				cudaDeviceSynchronize(); \
+				auto end = std::chrono::steady_clock::now(); \
+				auto cudaTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start); \
+				std::cout << "Kernel elapsed time: " << cudaTime.count() << "\n";
+#else
+#define RUN_ESCAPE_TIME_KERNEL escapeTime<<<blocks, threads>>> (cudaEscapeTimes, maxIters, sizeX, sizeY, scale, panX, panY); \
+				cudaDeviceSynchronize();
+#endif
