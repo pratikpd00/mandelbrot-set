48fe7aad5cdbdd3db705943a50b831dc1c6c5f7d
pratik <pratikpd00@gmail.com>
2025-01-01 17:18:55 -0500

    Add a CPU version of the mandelbrotImageTransformGrid

---
+++ lib/escapeTime/mandelbrotImageTransformGrid.cpp
@@ -0,0 +1,65 @@
+#include "escapeTime/mandelbrotImageTransformGrid.h"
+#include "escapeTimeSequential.h"
+
+
+
+void CpuMandelbrotImageTransformGrid::updateGrid() {
+	//call the sequential escape time function
+	escapeTimeSequential(colorGrid, maxIters, sizeX, sizeY, scale, startX, startY, ColoringFunctionType::DEFAULT);
+}
+
+CpuMandelbrotImageTransformGrid::CpuMandelbrotImageTransformGrid(uint sizeX, uint sizeY, uint maxIters, double scale, double startX, double startY) {
+	this->sizeX = sizeX;
+	this->sizeY = sizeY;
+	this->maxIters = maxIters;
+	this->scale = scale;
+	this->startX = startX;
+	this->startY = startY;
+	this->coloringFunction = ColoringFunctionType::DEFAULT;
+	colorGrid = std::vector<RGBColor>(sizeX * sizeY);
+
+	updateGrid();
+}
+
+RGBColor CpuMandelbrotImageTransformGrid::get(int x, int y)  {
+	//for the edge case where x or y is out of bounds, return black
+	if (x < 0 || x >= sizeX || y < 0 || y >= sizeY) return color(0, 0, 0);
+	return colorGrid[x * sizeY + y];
+}
+
+void CpuMandelbrotImageTransformGrid::zoom(double scale, int centerX, int centerY)  {
+	auto scaleChange = this->scale - scale;
+	auto xOffset = centerX * scaleChange;
+	auto yOffset = centerY * scaleChange;
+	this->scale = scale;
+	startY += yOffset;
+	startX += xOffset;
+	updateGrid();
+}
+
+void CpuMandelbrotImageTransformGrid::resizeGrid(uint sizeX, uint sizeY) {
+	if (sizeX * sizeY > colorGrid.size()) {
+		colorGrid.resize(sizeX * sizeY);
+	}
+
+	this->sizeX = sizeX;
+	this->sizeY = sizeY;
+
+	updateGrid();
+}
+
+void CpuMandelbrotImageTransformGrid::translate(double offsetX, double offsetY) {
+	startY -= offsetY * scale;
+	startX -= offsetX * scale;
+	updateGrid();
+}
+
+void CpuMandelbrotImageTransformGrid::setColoring(ColoringFunctionType func) {
+	coloringFunction = func;
+	updateGrid();
+}
+
+ColoringFunctionType CpuMandelbrotImageTransformGrid::getColoring() {
+	return coloringFunction;
+}
+ 
--- lib/escapeTime/test/imageTransformGridTests.cpp
+++ lib/escapeTime/test/imageTransformGridTests.cpp
@@ -7,7 +7,7 @@
 namespace ImageTransformGridTests {
 	TEST(testCudaGrid, InitializeGrid) {
 		auto dim = 5;
-		MandelbrotImageTransformGrid grid(dim, dim, 100, 0.1, 0.0, 0.0);
+		CudaMandelbrotImageTransformGrid grid(dim, dim, 100, 0.1, 0.0, 0.0);
 		std::vector<RGBColor> rawGrid(dim * dim);
 		escapeTimeSequential(rawGrid, 100, dim, dim, 0.1, 0.0, 0.0, ColoringFunctionType::DEFAULT);
 
@@ -21,7 +21,7 @@ namespace ImageTransformGridTests {
 
 	TEST(testCudaGrid, InitializeLargeGrid) {
 		auto dim = 1000;
-		MandelbrotImageTransformGrid grid(dim, dim, 100, 0.1, 0.0, 0.0);
+		CudaMandelbrotImageTransformGrid grid(dim, dim, 100, 0.1, 0.0, 0.0);
 		std::vector<RGBColor> rawGrid(dim * dim);
 		escapeTimeSequential(rawGrid, 100, dim, dim, 0.1, 0.0, 0.0, ColoringFunctionType::DEFAULT);
 
@@ -36,7 +36,7 @@ namespace ImageTransformGridTests {
 	TEST(testCudaGrid, InitializeRectangleGrid) {
 		auto dimx = 1000;
 		auto dimy = 500;
-		MandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
+		CudaMandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
 		std::vector<RGBColor> rawGrid(dimx * dimy);
 		escapeTimeSequential(rawGrid, 100, dimx, dimy, 1.0, 0.0, 0.0, ColoringFunctionType::DEFAULT);
 
@@ -51,7 +51,7 @@ namespace ImageTransformGridTests {
 	TEST(testCudaGrid, InitializeWithParameters) {
 		auto dimx = 1000;
 		auto dimy = 500;
-		MandelbrotImageTransformGrid grid(dimx, dimy, 100, 4.0, 0.5, -0.5);
+		CudaMandelbrotImageTransformGrid grid(dimx, dimy, 100, 4.0, 0.5, -0.5);
 		std::vector<RGBColor> rawGrid(dimx * dimy);
 		escapeTimeSequential(rawGrid, 100, dimx, dimy, 4.0, 0.5, -0.5, ColoringFunctionType::DEFAULT);
 
@@ -67,7 +67,7 @@ namespace ImageTransformGridTests {
 	TEST(testCudaGrid, Zoom) {
 		auto dimx = 1000;
 		auto dimy = 500;
-		MandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
+		CudaMandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
 		grid.zoom(2.0, 0.0, 0.0);
 		std::vector<RGBColor> rawGrid(dimx * dimy);
 		escapeTimeSequential(rawGrid, 100, dimx, dimy, 2.0, 0.0, 0.0, ColoringFunctionType::DEFAULT);
@@ -82,7 +82,7 @@ namespace ImageTransformGridTests {
 	TEST(testCudaGrid, MultiZoom) {
 		auto dimx = 1000;
 		auto dimy = 500;
-		MandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
+		CudaMandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
 		grid.zoom(2.0, 0.0, 0.0);
 		grid.zoom(3.0, 0.0, 0.0);
 		std::vector<RGBColor> rawGrid(dimx * dimy);
@@ -98,7 +98,7 @@ namespace ImageTransformGridTests {
 	TEST(testCudaGrid, OffsetZoom) {
 		auto dimx = 20;
 		auto dimy = 10;
-		MandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
+		CudaMandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
 		grid.zoom(2.5, 20, 10);
 		std::vector<RGBColor> rawGrid(dimx * dimy);
 		escapeTimeSequential(rawGrid, 100, dimx, dimy, 2.5, -30.0, -15.0, ColoringFunctionType::DEFAULT);
@@ -114,7 +114,7 @@ namespace ImageTransformGridTests {
 	TEST(testCudaGrid, Pan) {
 		auto dimx = 20;
 		auto dimy = 10;
-		MandelbrotImageTransformGrid grid(dimx, dimy, 100, 0.1, 0.0, 0.0);
+		CudaMandelbrotImageTransformGrid grid(dimx, dimy, 100, 0.1, 0.0, 0.0);
 		grid.translate(-1, -2);
 		std::vector<RGBColor> rawGrid(dimx * dimy);
 		escapeTimeSequential(rawGrid, 100, dimx, dimy, 0.1, 0.1, 0.2, ColoringFunctionType::DEFAULT);
@@ -130,7 +130,7 @@ namespace ImageTransformGridTests {
 	TEST(testCudaGrid, MultiPan) {
 		auto dimx = 20;
 		auto dimy = 10;
-		MandelbrotImageTransformGrid grid(dimx, dimy, 100, 0.1, 0.0, 0.0);
+		CudaMandelbrotImageTransformGrid grid(dimx, dimy, 100, 0.1, 0.0, 0.0);
 		grid.translate(-1, -2);
 		grid.translate(-1, 4);
 		std::vector<RGBColor> rawGrid(dimx * dimy);
@@ -146,7 +146,7 @@ namespace ImageTransformGridTests {
 	TEST(testCudaGrid, Resize) {
 		auto dimx = 1000;
 		auto dimy = 500;
-		MandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
+		CudaMandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
 		grid.resizeGrid(100, 100);
 		std::vector<RGBColor> rawGrid(dimx * dimy);
 		escapeTimeSequential(rawGrid, 100, 100, 100, 1.0, 0.0, 0.0, ColoringFunctionType::DEFAULT);
@@ -161,7 +161,7 @@ namespace ImageTransformGridTests {
 	TEST(testCudaGrid, ResizeLarger) {
 		auto dimx = 1000;
 		auto dimy = 500;
-		MandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
+		CudaMandelbrotImageTransformGrid grid(dimx, dimy, 100, 1.0, 0.0, 0.0);
 		grid.resizeGrid(600, 1100);
 		std::vector<RGBColor> rawGrid(600 * 1100);
 		escapeTimeSequential(rawGrid, 100, 600, 1100, 1.0, 0.0, 0.0, ColoringFunctionType::DEFAULT);
